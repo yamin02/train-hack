f767a7739d4c1abd7efe15fa919ee1ff39337d86
yamin02 <yamin136635@gmail.com>
2024-07-11 21:59:11 +0600

    done with train heck

--- hack.js
+++ hack.js
@@ -3,7 +3,6 @@ const puppeteer = require("puppeteer-core");
 const routes = ["Dhaka", "Chattogram"];
 const date = "17-Jul-2024" ;
 const profileInfo = require('./profile.json') ;
-// var [mobile , password ] = [ profileInfo.mobile , profileInfo.password]
 
 (async (info) => {
   const browser = await puppeteer.launch({
@@ -20,30 +19,39 @@ const profileInfo = require('./profile.json') ;
 
   const url = `https://eticket.railway.gov.bd/booking/train/search?fromcity=${routes[1]}&tocity=${routes[0]}&doj=${date}&class=SNIGDHA`;
 
-  for(var i = 1 ; i < 60 ; i=i+4){
-    var bogieName = 'THO';
-    var selector = '#search_list_sec > div.container > div > div.col-lg-9.col-md-8.col-sm-12.col-xs-12.px-xs-1 > app-single-trip:nth-child(7) > div > div.trip-collapsible.trip-div.trip-collapsed > div.seat-classes-row.d-flex > div:nth-child(2) > div.seat-availability-box > div.book-now-btn-wrapper.ng-star-inserted > button'
+  // for(var i = 1 ; i <= 60 ; i=i+4){
+  var i = 1 ;
+  while(true){  
+    var selectorBtn = '#search_list_sec > div.container > div > div.col-lg-9.col-md-8.col-sm-12.col-xs-12.px-xs-1 > app-single-trip:nth-child(7) > div > div.trip-collapsible.trip-div.trip-collapsed > div.seat-classes-row.d-flex > div:nth-child(2) > div.seat-availability-box > div.book-now-btn-wrapper.ng-star-inserted > button'
+    
     var page2 = await browser.newPage()
+    page2.setDefaultTimeout(3*1000)
     await page2.goto(url)
-    .then(() => page2.waitForSelector(".book-now-btn"))
+    .then(() => page2.waitForSelector(selectorBtn))
     // Take the selector from developer options Copy > Copy Selector
-    .then(() => page2.click(selector))
+    .then(() => page2.click(selectorBtn))
     .then(()=> page2.waitForSelector('#select-bogie'))   
     // this is to select bogie number from dropdown list (ka, kha , ...)
-    .then(() => page2.select('#select-bogie', '7'))      
-    .then(() => page2.waitForSelector(`[Title="${bogieName}-27"]`))
-    .then(() => Promise.all([
-      page2.click(`[Title="${bogieName}-${i}"]`),
-      page2.click(`[Title="${bogieName}-${i+1}"]`),
-      page2.click(`[Title="${bogieName}-${i+2}"]`),
-      page2.click(`[Title="${bogieName}-${i+3}"]`)
+    .then(() => page2.select('#select-bogie', '7')) 
+    .then(() => page2.waitForSelector(`.btn-seat.seat-available`))     
+    .then(() => page2.$$(`.btn-seat.seat-available`))
+    .then((seatBtn) => Promise.all([
+      seatBtn[0].click().catch(e => console.log('Aree sob booked')) ,
+      seatBtn[1].click().catch(e => console.log('Aree sob booked')) ,
+      seatBtn[2].click().catch(e => console.log('Aree sob booked')) ,
+      seatBtn[3].click().catch(e => console.log('Aree sob booked')) ,
     ]))
-    .catch(e => console.log(e));
+    .then(()=> page2.close() )
+    .catch(e => {
+      // console.log(e);
+      page2.close();
+      console.log('Ektu Blunder Hoye Gelo Dada')
+  });
+    i = (i==60) ? 1 :i+4 
   }
   
 
   console.log("Script executed successfully");
 
-  // Uncomment the following line to close the browser when done
   // await browser.close();
 })(profileInfo);
