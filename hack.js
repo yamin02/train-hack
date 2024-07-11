043d6dd6a42f06ab734ad6e2db0c720962bf4213
yamin02 <yamin136635@gmail.com>
2024-07-11 20:50:25 +0600

    secured the password

--- hack.js
+++ hack.js
@@ -1,8 +1,11 @@
 
 const puppeteer = require("puppeteer-core");
 const routes = ["Dhaka", "Chattogram"];
+const date = "17-Jul-2024" ;
+const profileInfo = require('./profile.json') ;
+// var [mobile , password ] = [ profileInfo.mobile , profileInfo.password]
 
-(async () => {
+(async (info) => {
   const browser = await puppeteer.launch({
     executablePath: "/usr/bin/chromium",
     headless: false,
@@ -10,29 +13,30 @@ const routes = ["Dhaka", "Chattogram"];
 
   const page = await browser.newPage();
   await page.goto("https://eticket.railway.gov.bd/login")
-    .then(() => page.type("#mobile_number", "01818672900"))
-    .then(() => page.type("#password", "*****"))
+    .then(() => page.type("#mobile_number", profileInfo.mobile ))
+    .then(() => page.type("#password",  profileInfo.password ))
     .then(() => page.click(".login-form-submit-btn"))
     .then(() => page.waitForSelector(".railway-ticket-search-submit-btn"));
 
-  const url = `https://eticket.railway.gov.bd/booking/train/search?fromcity=${routes[0]}&tocity=${routes[1]}&doj=09-Jul-2024&class=SNIGDHA`;
+  const url = `https://eticket.railway.gov.bd/booking/train/search?fromcity=${routes[1]}&tocity=${routes[0]}&doj=${date}&class=SNIGDHA`;
 
-  for(var i = 1 ; i < 50 ; i=i+4){
+  for(var i = 1 ; i < 60 ; i=i+4){
+    var bogieName = 'THO';
+    var selector = '#search_list_sec > div.container > div > div.col-lg-9.col-md-8.col-sm-12.col-xs-12.px-xs-1 > app-single-trip:nth-child(7) > div > div.trip-collapsible.trip-div.trip-collapsed > div.seat-classes-row.d-flex > div:nth-child(2) > div.seat-availability-box > div.book-now-btn-wrapper.ng-star-inserted > button'
     var page2 = await browser.newPage()
     await page2.goto(url)
     .then(() => page2.waitForSelector(".book-now-btn"))
-    .then(() => page2.$$(".book-now-btn"))
-    .then(bookbtn => {
-      bookbtn[6].click();
-      return page2.waitForSelector("#select-bogie");
-    })
-    .then(() => page2.select('#select-bogie', '4'))
-    .then(() => page2.waitForSelector('[Title="NEO-27"]'))
+    // Take the selector from developer options Copy > Copy Selector
+    .then(() => page2.click(selector))
+    .then(()=> page2.waitForSelector('#select-bogie'))   
+    // this is to select bogie number from dropdown list (ka, kha , ...)
+    .then(() => page2.select('#select-bogie', '7'))      
+    .then(() => page2.waitForSelector(`[Title="${bogieName}-27"]`))
     .then(() => Promise.all([
-      page2.click(`[Title="NEO-${i}"]`),
-      page2.click(`[Title="NEO-${i+1}"]`),
-      page2.click(`[Title="NEO-${i+2}"]`),
-      page2.click(`[Title="NEO-${i+3}"]`)
+      page2.click(`[Title="${bogieName}-${i}"]`),
+      page2.click(`[Title="${bogieName}-${i+1}"]`),
+      page2.click(`[Title="${bogieName}-${i+2}"]`),
+      page2.click(`[Title="${bogieName}-${i+3}"]`)
     ]))
     .catch(e => console.log(e));
   }
@@ -42,4 +46,4 @@ const routes = ["Dhaka", "Chattogram"];
 
   // Uncomment the following line to close the browser when done
   // await browser.close();
-})();
+})(profileInfo);
