b4161f16db18c6985fb9112cc6236d4c4919b8d8
Yaminul Hoque <61344650+yamin02@users.noreply.github.com>
2024-12-01 12:39:35 +0600

    Update hack-windows.js

--- hack-windows.js
+++ hack-windows.js
@@ -19,8 +19,8 @@ function delay(ms) {
     const page = await browser.newPage();
     await page
       .goto("https://eticket.railway.gov.bd/login")
-      .then(() => page.type("#mobile_number", "01818672900"))
-      .then(() => page.type("#password", "chandanpura"))
+      .then(() => page.type("#mobile_number", "[]"))
+      .then(() => page.type("#password", "[]"))
       .then(() => page.click(".login-form-submit-btn"))
       .then(() => page.waitForSelector(".railway-ticket-search-submit-btn"))
       .then(() => console.log("Login done Successfully"));
